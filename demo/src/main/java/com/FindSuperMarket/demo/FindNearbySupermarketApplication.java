223b49dd7668762d48dbdaf2c9aaa57c68e6a743
Trung Kiên <131913502+anhkienit3011@users.noreply.github.com>
2024-06-25 11:18:12 +0700

    base

---
+++ demo/src/main/java/com/FindSuperMarket/demo/FindNearbySupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.FindSuperMarket.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindNearbySupermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindNearbySupermarketApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/FindSuperMarket/demo/FindNearbySupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FindSuperMarket.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindNearbySupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
