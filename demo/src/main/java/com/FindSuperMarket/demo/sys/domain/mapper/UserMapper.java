7c6be48d865e9bb5a8cbf0864f8df01a51b5f2b9
Trung Kiên <131913502+anhkienit3011@users.noreply.github.com>
2024-06-26 11:56:25 +0700

    update

--- demo/src/main/java/com/FindSuperMarket/demo/sys/domain/mapper/UserMapper.java
+++ demo/src/main/java/com/FindSuperMarket/demo/sys/domain/mapper/UserMapper.java
@@ -1,28 +1,29 @@
 package com.FindSuperMarket.demo.sys.domain.mapper;
 
 import com.FindSuperMarket.demo.sys.domain.dto.UserDTO;
+import com.FindSuperMarket.demo.sys.domain.dto.UserLocationDTO;
 import com.FindSuperMarket.demo.sys.domain.entity.User;
+import com.FindSuperMarket.demo.sys.domain.entity.UserLocation;
 import com.FindSuperMarket.demo.sys.domain.request.user.UserRegistrationRequest;
-import org.mapstruct.*;
-
-import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    @Mapping(target = "location", ignore = true)
-    UserDTO toDTO(User user);
-
-    List<UserDTO> toDTOList(List<User> users);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "passwordHash", ignore = true)
     @Mapping(target = "location", ignore = true)
     User toEntity(UserRegistrationRequest request);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    UserDTO toDTO(User user);
+
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "passwordHash", ignore = true)
     @Mapping(target = "location", ignore = true)
     void updateEntityFromRequest(UserRegistrationRequest request, @MappingTarget User user);
+
+    @Mapping(source = "user.id", target = "userId")
+    UserLocationDTO toDTO(UserLocation userLocation);
 }
